32d8b9306294a4ed56837efb7fc634380eb6f9d3
Makenzie Buchenroth <mbuchenroth@student.neumont.edu>
2024-03-05 17:21:49 -0700

    Added main menu.

--- Assets/Assets/Scripts/GameManager.cs
+++ Assets/Assets/Scripts/GameManager.cs
@@ -2,42 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    enum State {
-        STARTSCREEN,
-        STARTGAME,
-        GAME,
-        DAYCHANGE
-    }
+public class GameManager : MonoBehaviour {
+	[SerializeField] GameObject menuUI;
 
-    State currentState = State.GAME;
+	enum State {
+		STARTSCREEN,
+		STARTGAME,
+		GAME,
+		DAYCHANGE
+	}
 
-    private void Update() {
-        switch (currentState) {
-            case State.STARTSCREEN:
-                break;
-            case State.STARTGAME:
-                break;
-            case State.GAME:
-                //test move day forward
-                if (Input.GetKeyDown(KeyCode.O)) {
-                    DayChange();
-                }
-                break;
-            case State.DAYCHANGE:
-                var plants = GameObject.FindGameObjectsWithTag("Plant");
-                foreach (var plant in plants) {
-                    if (plant.TryGetComponent(out Plant plantScript)) {
-                        plantScript.setActiveSprite();
-                    }
-                }
-                currentState = State.GAME;
-                break;
-        }
-    }
+	State currentState = State.GAME;
 
-    public void DayChange() {
-        currentState = State.DAYCHANGE;
-    }
+	private void Update() {
+		switch (currentState) {
+			case State.STARTSCREEN:
+				menuUI.SetActive(true);
+				break;
+			case State.STARTGAME:
+				menuUI.SetActive(false);
+				break;
+			case State.GAME:
+				//test move day forward
+				if (Input.GetKeyDown(KeyCode.O)) {
+					DayChange();
+				}
+				break;
+			case State.DAYCHANGE:
+				var plants = GameObject.FindGameObjectsWithTag("Plant");
+				foreach (var plant in plants) {
+					if (plant.TryGetComponent(out Plant plantScript)) {
+						plantScript.setActiveSprite();
+					}
+				}
+				currentState = State.GAME;
+				break;
+		}
+	}
+
+	public void DayChange() {
+		currentState = State.DAYCHANGE;
+	}
+
+	public void startGame() {
+		currentState = State.STARTGAME;
+		// later make it go to save selection
+	}
+
+	public void newSave() {
+		currentState = State.STARTGAME;
+		// later make it go to player creation
+	}
+
+	public void exitGame() {
+		Application.Quit();
+	}
 }
